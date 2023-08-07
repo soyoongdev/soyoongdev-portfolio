68f12555486b536734883e071746e7a39acdd014
soyoongdev <huuhau.hh47@gmail.com>
2023-08-07 17:12:17 +0700

    fix: remove path index to build source

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as RowContent } from './ui/row-content'
-export { default as RowTitle } from './ui/row-title'
--- src/components/layouts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from './navbar'
-export { default as Footer } from './footer'
-export * as Wrapper from './wrapper'
